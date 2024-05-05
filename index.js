196068846abf1d416690f0ba3749166c6143ca62
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-05 18:03:30 +0600

    send token to client from server and set it to the cookie

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middlewares
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}))
 app.use(express.json())
 
 
@@ -30,6 +37,20 @@ async function run() {
         const database = client.db("bookDB");
         const bookCollection = database.collection("books");
         const addNewBookCollection = database.collection("addNewBooks");
+
+        // auth or jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log('User for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none'
+                })
+                .send({ success: true });
+        })
 
         // Find all (10) books data
         app.get('/books/all', async (req, res) => {
