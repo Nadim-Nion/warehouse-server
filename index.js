85dbf80e6eefe1d60df822e818bf811e822cf792
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-08 21:54:11 +0600

    get the books count

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.use(cors({
         'http://localhost:5173'
     ],
     credentials: true
-}))
-app.use(express.json())
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qf8hqc8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,6 +30,26 @@ const client = new MongoClient(uri, {
     }
 });
 
+//Custom Middlewares
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('Token in the Cookies inside middleware:', token);
+
+    // if no token available (expired or deleted)
+    if (!token) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+    // verify jwt token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            res.status(401).send({ message: "unauthorized access" });
+        }
+        req.user = decoded;
+        next();
+    })
+    // next();
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -41,7 +62,7 @@ async function run() {
         // auth or jwt related api
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            console.log('User for token', user);
+            // console.log('User for token', user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res
                 .cookie('token', token, {
@@ -66,6 +87,12 @@ async function run() {
             const cursor = bookCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        // Get the total count in  book collection
+        app.get('/booksCount', async (req, res) => {
+            const total = await bookCollection.estimatedDocumentCount();
+            res.send({ total });
         })
 
         // Find the first six books data
@@ -114,8 +141,18 @@ async function run() {
         })
 
         // View (GET) the all newly added books by user email
-        app.get('/newAddedBooks', async (req, res) => {
-            const query = req.params.email;
+        app.get('/newAddedBooks', verifyToken, async (req, res) => {
+            // const query = req.query.email;
+            console.log(req.query.email);
+            // console.log('Cookies', req.cookies);
+            // console.log('Token owner info:', req.user);
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: "forbidden access" });
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email };
+            }
             const cursor = addNewBookCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
